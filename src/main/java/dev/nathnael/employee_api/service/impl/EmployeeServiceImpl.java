97614928328e2dcfb03b08fb39e801daea93d1db
nathnael <134805961+nchanyal@users.noreply.github.com>
2026-07-24 17:41:37 -0400

    add endpoint to update employees

--- src/main/java/dev/nathnael/employee_api/service/impl/EmployeeServiceImpl.java
+++ src/main/java/dev/nathnael/employee_api/service/impl/EmployeeServiceImpl.java
@@ -30,7 +30,9 @@ public class EmployeeServiceImpl implements EmployeeService {
     @Override
     public EmployeeDto getEmployeeById(Long employeeId) {
         
-        Employee employee = employeeRepository.findById(employeeId).orElseThrow(() -> new ResourceNotFoundException("Employee does not exist with given id:" + employeeId));
+        Employee employee = employeeRepository.findById(employeeId).orElseThrow(
+            () -> new ResourceNotFoundException("Employee does not exist with given id:" + employeeId)
+        );
         return EmployeeMapper.mapToEmployeeDto(employee);
     }
 
@@ -38,6 +40,22 @@ public class EmployeeServiceImpl implements EmployeeService {
     public List<EmployeeDto> getAllEmployees() {
         List<Employee> employees = employeeRepository.findAll();
         return employees.stream().map((employee) -> EmployeeMapper.mapToEmployeeDto(employee)).collect(Collectors.toList());
+    }
+
+    @Override
+    public EmployeeDto updateEmployee(Long employeeId, EmployeeDto updatedEmployeeDto) {
+        
+        Employee employee = employeeRepository.findById(employeeId).orElseThrow(
+            () -> new ResourceNotFoundException("Employee does not exist with given id:" + employeeId)
+        );
+
+        employee.setFirstName(updatedEmployeeDto.getFirstName());
+        employee.setLastName(updatedEmployeeDto.getLastName());
+        employee.setEmail(updatedEmployeeDto.getEmail());
+
+        Employee updatedEmployee = employeeRepository.save(employee);
+
+        return EmployeeMapper.mapToEmployeeDto(updatedEmployee);
     }
     
 }

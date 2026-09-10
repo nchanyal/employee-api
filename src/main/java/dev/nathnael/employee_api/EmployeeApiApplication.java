3da145a679f93826acced198d690f94edceb2d39
nathnael <134805961+nchanyal@users.noreply.github.com>
2026-09-10 15:16:31 -0400

    add command line runner to populate employee table

--- src/main/java/dev/nathnael/employee_api/EmployeeApiApplication.java
+++ src/main/java/dev/nathnael/employee_api/EmployeeApiApplication.java
@@ -1,10 +1,17 @@
 package dev.nathnael.employee_api;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
 import dev.nathnael.employee_api.config.RsaKeyProperties;
+import dev.nathnael.employee_api.entity.Employee;
+import dev.nathnael.employee_api.repository.EmployeeRepository;
 
 @SpringBootApplication
 @EnableConfigurationProperties(RsaKeyProperties.class)
@@ -12,6 +19,29 @@ public class EmployeeApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeApiApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner seedDatabase(EmployeeRepository employeeRepository) {
+		return args -> {
+			if (employeeRepository.count() == 0) {
+				for(int i = 0; i < 25; i++) {
+					Employee employee = new Employee();
+
+					employee.setFirstName("Employee");
+					employee.setLastName("Test" + i);
+					employee.setEmail("employee" + i + "@example.com");
+					employee.setDepartment("IT");
+					employee.setJobTitle("Software Engineer");
+					employee.setSalary(new BigDecimal("60000.00").add(
+							new BigDecimal(i * 100)
+					));
+					employee.setHireDate(LocalDate.of(2026, 9, 1));
+
+					employeeRepository.save(employee);
+				}
+			}
+		};
 	}
 
 }
